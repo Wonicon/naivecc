1b910c65eecd32de5f0b9ea26f4c22ccb06e82d9
Whz <huizhewang@outlook.com>
2015-09-17 00:17:46 -0700

    Fix mistake

--- token.h
+++ token.h
@@ -29,7 +29,7 @@ enum TOKEN_TYPE
     TYPE,      // int | float
     LP,        // (
     RP,        // )
-    LB         // [
+    LB,        // [
     RB,        // ]
     LC,        // {
     RC,        // }
