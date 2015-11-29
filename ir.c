b1b3b2048f361eeabb43f5529afc4d31038cf776
Wonicon <conciselove@outlook.com>
2015-11-29 23:11:35 +0800

    Misc.

--- ir.c
+++ ir.c
@@ -145,13 +145,6 @@ void print_instr(FILE *file)
     }
 #endif
 
-    // 第二趟
-    memcpy(instr_buffer, ir_from_dag, sizeof(ir_from_dag));
-    nr_instr = nr_ir_from_dag;
-    memset(ir_from_dag, 0, sizeof(ir_from_dag));
-    nr_ir_from_dag = 0;
-    print_block();
-
     inline_replace(ir_from_dag, nr_ir_from_dag);
 
     for (int i = 0; i < nr_ir_from_dag; i++) {
